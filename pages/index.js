1a953e3554aa15f6511299e432f4bbeeceb6906f
yupi1 <toxahitech@gmail.com>
2021-05-18 12:12:52 +0300

    create component system

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,9 @@
-import Head from 'next/head'
+import Navigation from "./../components/Navigation.js";
 
 export default function HomePage() {
   return (
     <div className="container">
-      <nav className="navigation-menu">
-        <ul className="nav-list">
-          <li className="nav-item"><a className="nav-link" href="">About</a></li>
-          <li className="nav-item"><a className="nav-link" href="">Services</a></li>
-          <li className="nav-item"><a className="nav-link" href="">Contact</a></li>
-        </ul>
-      </nav>
+      <Navigation />
       <h1 className="red">Wood Export from Ukraine</h1>
     </div>
   )
