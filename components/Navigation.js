43cd72f5f7c0083b2bda61cec9c8ad2e25bf6b3b
yupi1 <toxahitech@gmail.com>
2021-05-18 12:13:28 +0300

    Navigation redirect

---
+++ components/Navigation.js
@@ -0,0 +1,11 @@
+export default function Navigation() {
+  return (
+    <nav className="navigation-menu">
+      <ul className="nav-list">
+        <li className="nav-item"><a className="nav-link" href="">About</a></li>
+        <li className="nav-item"><a className="nav-link" href="">Services</a></li>
+        <li className="nav-item"><a className="nav-link" href="">Contact</a></li>
+      </ul>
+    </nav>
+  )
+}
