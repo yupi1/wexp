76e2b3efdec6a85ef8c1b5cc30f5e6db9089b1bc
yupi1 <toxahitech@gmail.com>
2021-05-19 11:40:20 +0300

    add about files

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,21 @@
 import Navigation from "./../components/Navigation.js";
+import About from "./../components/About.js"
 
 export default function HomePage() {
   return (
     <div className="container">
-      <Navigation />
-      <main>
-        <h1 className="red">We are export</h1>
+      <header>
+        <Navigation />
+        <span style={{visibility: "hidden"}}>DT</span>
+      </header>
 
+      <main>
+        <About />
+
+        <section className="Services"></section>
       </main>
+
+      <footer className="contact"></footer>
     </div>
   )
 }
