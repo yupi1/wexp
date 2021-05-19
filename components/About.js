e03f21a2d5141216c974f9c4c9d7174ac4ba2549
yupi1 <toxahitech@gmail.com>
2021-05-19 11:38:41 +0300

    add About files

---
+++ components/About.js
@@ -0,0 +1,10 @@
+import styles from '../styles/About.module.css'
+
+export default function About() {
+  return (
+    <section className="About">
+      <h1 className="red">We are exporter</h1>
+
+    </section>
+  )
+}
