6c56349d75aaa884665314209ce5f81808ebf240
yupi1 <toxahitech@gmail.com>
2021-06-04 16:49:14 +0300

    for deploy change

--- components/Services.js
+++ components/Services.js
@@ -6,14 +6,14 @@ export default function Services({ forwardedRef }) {
 
   return (
     <section className={styles.services} ref={forwardedRef}>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit.Nihil possimus voluptates error facere quae odit quos harum ? Placeat, exercitationem in assumenda et omnis ea culpa perferendis possimus mollitia praesentium rerum ?
+      {/* Lorem ipsum dolor sit amet consectetur adipisicing elit.Nihil possimus voluptates error facere quae odit quos harum ? Placeat, exercitationem in assumenda et omnis ea culpa perferendis possimus mollitia praesentium rerum ?
       <Image
         src="/timber_4.jpg"
         width={400}
         height={600}
         objectFit="contain"
         layout="fixed"
-      />
+      /> */}
     </section >
   )
 }
