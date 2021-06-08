0d7167a273078bb9589b27ced2d148eba2277059
yupi1 <toxahitech@gmail.com>
2021-06-08 14:46:08 +0300

    add services content 2

--- components/Services.js
+++ components/Services.js
@@ -1,6 +1,5 @@
 import Image from 'next/image'
 import Lumber from '../public/lumber.svg'
-// import Logo from "../public/dia.svg";
 
 import styles from "../styles/Services.module.css"
 
@@ -13,13 +12,34 @@ export default function Services({ forwardedRef }) {
           <h2><span className={styles.text_gradient}>Softwood</span> and <span className={styles.text_gradient}>hardwood</span> timber for carpentry, building and construction industry</h2>
           <Lumber className={styles.img_lumber} />
         </div>
-        {/* <Image
-          src="/timber_5.jpg"
-          width={320}
-          height={500}
-          objectFit="contain"
-          layout="fixed"
-        /> */}
+
+        <div className={styles.section_production}>
+          <h3>Production Capacity</h3>
+
+          <div className={styles.wrapper}>
+            <div className={styles.image_container}>
+              <Image
+                src="/timber_3.jpg"
+                width={370}
+                height={550}
+                objectFit="contain"
+              />
+              <Image
+                className={styles.image_hero}
+                src="/timber_4.jpg"
+                width={240}
+                height={400}
+                objectFit="contain"
+              />
+            </div>
+            <div className={styles.text_container}>
+              <h4>wide range of products:</h4>
+              <p>From parquet building boards to edged and unedged boards</p>
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.section_devilery}></div>
       </div>
     </section >
   )
